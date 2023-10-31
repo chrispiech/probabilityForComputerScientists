63baca59487e2f81a77dfcc5375f0ef776e33ed3
Chris Piech <piech@cs.stanford.edu>
2023-10-30 17:43:12 -0700

    auto commit

--- print/printbook.py
+++ print/printbook.py
@@ -1,11 +1,13 @@
 import hjson
+import json
 import PyPDF2
 import pdfgenerator
 import re
 import os
 from tqdm import tqdm
+import hashlib
  
-# def create_pdfs():
+
 # Opening JSON file
 f = open("../bookOutline.hjson")
 # returns JSON object as a dictionary
@@ -31,77 +33,109 @@ with open(os.path.join('pdfs', title_name), "wb") as outfile:
 
 # get pdf_name and pdf_link for book from bookOutline and store in pdf_files
 pdf_files = {}
+# load the hash values
+# if the file does not exist, hash_values is {}
+if not os.path.exists('hash_values.json'):
+    hash_values = {}
+else:
+    hash_values = json.load(open('hash_values.json', 'r'))
 
-for part in tqdm(data, desc="Outer Loop"):
-    pdf_files[part] = {'sections':{}}
-    for page in data[part]['sections']:
-        title = data[part]['sections'][page]
-        pdf_name = page + '.pdf'
-        pdf_link = base + part + '/' + page 
-        # store pdf_name and title
-        pdf_files[part]['sections'][pdf_name] = title
-        # generate pdf file
-        pdf_file = pdfgenerator.PdfGenerator([pdf_link]).main()
-        # save pdf to file
-        with open(os.path.join('pdfs', pdf_name), "wb") as outfile:
-            outfile.write(pdf_file[0].getbuffer())
-            # print(pdf_name)
-    if 'examples' in data[part].keys():
-        pdf_files[part]['examples'] = {}
-        for page in data[part]['examples']:
-            title = data[part]['examples'][page]
+def print_page(part, page):
+    pdf_name = page + '.pdf'
+    pdf_link = base + part + '/' + page
+    file_link = f'../en/{part}/{page}/index.html'
+    raw_file_html = open(file_link, 'r').read()
+    hash = compute_md5(raw_file_html)
+    if file_link in hash_values.keys() and hash_values[file_link] == hash:
+        return
+    # store pdf_name and title
+    # generate pdf file
+    print(pdf_link)
+    pdf_file = pdfgenerator.PdfGenerator([pdf_link]).main()
+    # save pdf to file
+    with open(os.path.join('pdfs', pdf_name), "wb") as outfile:
+        outfile.write(pdf_file[0].getbuffer())
+        save_hash_value(hash, file_link)
+
+def print_book():
+    for part in tqdm(data, desc="Sections"):
+        pdf_files[part] = {'sections':{}}
+        for page in data[part]['sections']:
+            print_page(part, page)
+
+            # save for table of contents
+            title = data[part]['sections'][page]
             pdf_name = page + '.pdf'
-            pdf_link = base + 'examples' + '/' + page 
-            # store pdf_name and title
-            pdf_files[part]['examples'][pdf_name] = title
-            # generate pdf file
-            pdf_file = pdfgenerator.PdfGenerator([pdf_link]).main()
-            # save pdf to file
-            with open(os.path.join('pdfs', pdf_name), "wb") as outfile:
-                outfile.write(pdf_file[0].getbuffer())
-                # print(pdf_name)
+            pdf_files[part]['sections'][pdf_name] = title
+        if 'examples' in data[part].keys():
+            pdf_files[part]['examples'] = {}
+            for page in data[part]['examples']:
+                print_page('examples', page)
 
-# Output PDF file name
-output_pdf = "../en/ProbabilityForComputerScientists.pdf"
+                # save for table of contents
+                title = data[part]['examples'][page]
+                pdf_name = page + '.pdf'
+                pdf_files[part]['examples'][pdf_name] = title
 
-# Create a PDF file writer object
-pdf_writer = PyPDF2.PdfWriter()
 
-# add title page
-pdf_writer.append(os.path.join('pdfs', title_name))
+    # Output PDF file name
+    output_pdf = "../en/ProbabilityForComputerScientists.pdf"
 
-page_num = 1 
-for part in pdf_files:
-    title = data[part]['title']
-    if title is None:
-        title = "Introduction"
-    # create outline for parts
-    part_outline = pdf_writer.add_outline_item(title, page_num)
-    # add pdf files to table of contents and book
-    for pdf_file, title in pdf_files[part]['sections'].items():
-        # Open the pdf
-        pdf_reader = PyPDF2.PdfReader(open(os.path.join('pdfs', pdf_file), "rb"))
-        # Create an outline item for the pdf
-        pdf_outline = pdf_writer.add_outline_item(title, page_num, parent=part_outline)
-        # add pdf file to book pdf 
-        pdf_writer.append(os.path.join('pdfs', pdf_file))
-        # start of next pdf 
-        page_num += len(pdf_reader.pages)
-    if 'examples' in pdf_files[part].keys():
-        # create outline for examples
-        examples_outline = pdf_writer.add_outline_item('Applications', page_num, parent=part_outline)
+    # Create a PDF file writer object
+    pdf_writer = PyPDF2.PdfWriter()
+
+    # add title page
+    pdf_writer.append(os.path.join('pdfs', title_name))
+
+    page_num = 1 
+    for part in pdf_files:
+        title = data[part]['title']
+        if title is None:
+            title = "Introduction"
+        # create outline for parts
+        part_outline = pdf_writer.add_outline_item(title, page_num)
         # add pdf files to table of contents and book
-        for pdf_file, title in pdf_files[part]['examples'].items():
+        for pdf_file, title in pdf_files[part]['sections'].items():
             # Open the pdf
             pdf_reader = PyPDF2.PdfReader(open(os.path.join('pdfs', pdf_file), "rb"))
             # Create an outline item for the pdf
-            pdf_outline = pdf_writer.add_outline_item(title, page_num, parent=examples_outline)
+            pdf_outline = pdf_writer.add_outline_item(title, page_num, parent=part_outline)
+            # add pdf file to book pdf 
             pdf_writer.append(os.path.join('pdfs', pdf_file))
             # start of next pdf 
             page_num += len(pdf_reader.pages)
+        if 'examples' in pdf_files[part].keys():
+            # create outline for examples
+            examples_outline = pdf_writer.add_outline_item('Applications', page_num, parent=part_outline)
+            # add pdf files to table of contents and book
+            for pdf_file, title in pdf_files[part]['examples'].items():
+                # Open the pdf
+                pdf_reader = PyPDF2.PdfReader(open(os.path.join('pdfs', pdf_file), "rb"))
+                # Create an outline item for the pdf
+                pdf_outline = pdf_writer.add_outline_item(title, page_num, parent=examples_outline)
+                pdf_writer.append(os.path.join('pdfs', pdf_file))
+                # start of next pdf 
+                page_num += len(pdf_reader.pages)
 
-# Save the merged PDF with the TOC
-with open(output_pdf, "wb") as output_file:
-    pdf_writer.write(output_file)
+    # Save the merged PDF with the TOC
+    with open(output_pdf, "wb") as output_file:
+        pdf_writer.write(output_file)
 
-print(f"Merged PDF with Table of Contents saved as {output_pdf}")
+    print(f"Merged PDF with Table of Contents saved as {output_pdf}")
+
+def save_hash_value(hash, file_link):
+
+    # store the hash value
+    hash_values[file_link] = hash
+
+    # save the hash values to json
+    with open('hash_values.json', 'w') as fp:
+        json.dump(hash_values, fp, indent=4)
+
+def compute_md5(text):
+    m = hashlib.md5()
+    m.update(text.encode('utf-8'))
+    return m.hexdigest()
+
+if __name__ == "__main__":
+    print_book()
